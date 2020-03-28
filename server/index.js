bf5f12380dcf567c53ed2b0eb55c091f01c99b2d
Caleb Chang <changc42@gmail.com>
2020-03-28 16:11:16 -0400

    created client. attempting to get client to show up on server side in development

--- server/index.js
+++ server/index.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const app = express();
 
-if (process.env.NODE_ENV == "PRODUCTION") {
-  app.get("*", (req, res) => {
-    res.sendFile("./client/build/index.html");
-  });
-}
+// if (process.env.NODE_ENV == "PRODUCTION") {
+app.get("*", (req, res) => {
+  const path = require("path");
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+// }
+
+let PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("running on server ", PORT);
+});
