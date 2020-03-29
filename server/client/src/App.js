e148efc535a99ef20336b12e32ca69a7118e6f80
Caleb Chang <changc42@gmail.com>
2020-03-28 21:08:26 -0400

    succesfully deployed. react-scroll works too

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,11 +1,50 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
+import {
+  Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button,
+  Container
+} from "@material-ui/core";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Filler from "./Filler";
+import Scroll, { Element } from "react-scroll";
+import MenuIcon from "@material-ui/icons/Menu";
+const ScrollLink = Scroll.Link;
 
 function App() {
   return (
-    <div className="App">
-      <Typography variant="h1">corOHNO</Typography>
-    </div>
+    <BrowserRouter>
+      <Container>
+        <Route exact path="/">
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton>
+                <MenuIcon />
+              </IconButton>
+              <ScrollLink spy={true} smooth={true} duration={500} to="section4">
+                Section 4
+              </ScrollLink>
+            </Toolbar>
+          </AppBar>
+          <nav></nav>
+
+          <Filler />
+          <Filler />
+          <Filler />
+          <Element id="section4" name="section4">
+            <Filler />
+          </Element>
+        </Route>
+        <Route exact path="/1">
+          <h1>page1</h1>
+        </Route>
+        <Route exact path="/2">
+          <h1>page2</h1>
+        </Route>
+      </Container>
+    </BrowserRouter>
   );
 }
 
